9b232080005d0396e235d5d70b18b9dc823a50e6
Tyler <tyler@ogdenstudios.xyz>
2018-08-22 17:32:28 -0600

    Add line break to push

--- static/webhooks/push.php
+++ static/webhooks/push.php
@@ -1,6 +1,6 @@
 <?php
 chdir('/home/dh_ij9i3a/hugo.ssdp.org');
-$git_pull_output = shell_exec('git pull');
+$git_pull_output = shell_exec("git pull 2>&1");
 // Need to run gulp just once here, too.
-$hugo_output = shell_exec('../hugo');
+$hugo_output = shell_exec("../hugo 2>&1");
 ?>
