af3bc4d937e015825d05cffa7366896979932e8b
Tyler <tyler@ogdenstudios.xyz>
2018-08-22 17:45:52 -0600

    Update comments in push.ph

--- static/webhooks/push.php
+++ static/webhooks/push.php
@@ -1,6 +1,7 @@
 <?php
 chdir('/home/dh_ij9i3a/hugo.ssdp.org');
 $git_pull_output = shell_exec("git pull 2>&1");
-// Need to run gulp just once here, too.
+// We'll need to run gulp here as well, but for now we're getting errors
+// It's not mission-critical, since people won't likely be editing scss
 $hugo_output = shell_exec("../hugo 2>&1");
 ?>
